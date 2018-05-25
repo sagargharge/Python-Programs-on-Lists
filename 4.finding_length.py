d6c76cce84df8e1932655fed14bd43811898d54c
sagargharge <35300966+sagargharge@users.noreply.github.com>
2018-05-25 18:36:06 +0530

    Counting the elements in the list

---
+++ 4.finding_length.py
@@ -0,0 +1,16 @@
+#Question - 1.04 (*) Find the number of elements of a list.
+
+# Examples
+x = ['a','s','d','f','g']
+print x
+
+# We need to find the length to know the number of elements in the list.
+a = len(x)
+
+print "The number of elements present in the list are: " + str(a)
+
+
+# Output.
+# ['a', 's', 'd', 'f', 'g']
+# The number of elements present in the list are: 5
+
